c7d14b382c76d4b6ff1f661e1bd2c436133eac30
fishball0741 <fishball0741@gmail.com>
2022-09-14 23:28:16 +1000

    boss added

--- src/main.py
+++ src/main.py
@@ -1,2 +1,19 @@
 import zombie
 
+male = zombie.Character('Raymond', health=130, attack=10)
+female = zombie.Character('Becky', health=90, attack=20)
+item = zombie.Item('AK-47', 'Lv.3 Shield', 50)
+Boomer = zombie.Boss('LV.1_Boomer', health=200, attack=6)
+Hunter = zombie.Boss('LV.2_Hunter', health=150, attack= 20)
+Witch = zombie.Boss('LV.3_Witch', health=250, attack=25)
+Tank = zombie.Boss('Final_Tank', health=350, attack=30)
+
+
+
+print(male.__dict__)
+print(female.__dict__)
+print(item.__dict__)
+print(Boomer.__dict__)
+print(Hunter.__dict__)
+print(Witch.__dict__)
+print(Tank.__dict__)
--- src/zombie.py
+++ src/zombie.py
@@ -1,5 +1,5 @@
 class Item:
-    def __init__(self, weapon, gear, potions):
+    def __init__(self, weapon, gear, potions=50):
         self.weapon = weapon
         self.gear = gear
         self.potions = potions
@@ -10,3 +10,10 @@ class Character:
         self.name = name
         self.health = health
         self.attack = attack
+
+
+
+class Boss(Character):
+    def __init__(self, name, health=100, attack=10):
+        super().__init__(name, health=health, attack=attack)
+    
