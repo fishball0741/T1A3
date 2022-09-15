37705973c49f037213c3e6e63fa30ecd688d3a41
fishball0741 <fishball0741@gmail.com>
2022-09-16 00:47:51 +1000

    making battle round

--- src/main.py
+++ src/main.py
@@ -13,8 +13,12 @@ Tank = zombie.Boss('Final_Tank', health=350, attack=30)
 
 
 currentGame = zombie.game()
+player_health = [male.health, female.health]
+player_attack = [male.attack, female.attack]
+boss_health = [Boomer.health, Hunter.health, Witch.health, Tank.health]
+boss_attack = [Boomer.attack, Hunter.attack, Witch.attack, Tank.attack]
+# battle = [male, female, Boomer, Hunter, Witch, Tank]
 
-battle = [male, female, Boomer, Hunter, Witch, Tank]
 # print(male.__dict__)
 # print(female.__dict__)
 # print(item.__dict__)
@@ -23,7 +27,22 @@ battle = [male, female, Boomer, Hunter, Witch, Tank]
 # print(Witch.__dict__)
 # print(Tank.__dict__)
 
-print(f'Boomer got hit and still have {Boomer.damage(Tank)} hp left.')
+# print(f'Boomer got hit and still have {Boomer.damage(Tank)} hp left.')
+while player_health <= 0 and boss_health <= 0:
+    player_dmg = random.randint(10, player_attack)
+    boss_dmg = random.randint(0, boss_attack)
+    if player_health > 0:
+        boss_health -= player_dmg
+        print(f'Boomer got hit and still have {boss_health} hp left.')
+    elif boss_health > 0:
+        player_health -= boss_dmg
+        print(f'Male got hit and still have {player_health} hp left.')
+if player_health > 0 and boss_health <= 0:
+    win = True
+    print('Congrat, you WON !!! ')
+elif player_health <= 0 and boss_health > 0:
+    print('GAME OVER!!!')
+
 
 
 # print(Boomer.__dict__)
