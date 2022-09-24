e76daf49b23caf0e2b8ba651f5107f905f14f1a0
fishball0741 <fishball0741@gmail.com>
2022-09-24 21:00:14 +1000

    bug fixed

--- src/Choose.py
+++ src/Choose.py
@@ -5,13 +5,18 @@ import zombie
 # def 2 character for user to choose, and thier traits
 
 def choose_character(genderArg):
+    gender= ""
     if genderArg:
         if genderArg == "f":
             gender = 'female'
         else:
             gender = 'male'
     else:
-        gender = input("Please select your character, 'male' or 'female'? ")
+        while gender not in ('male', 'female'):
+            gender = input("Please select your character, 'male' or 'female'? ")
+        if not gender:
+            print("Sorry, I didn't catch that. Enter again: ")      
+    
     while True:
         name = input("Please write down your character's name: ")
     
@@ -23,7 +28,7 @@ def choose_character(genderArg):
         elif gender == 'female':
             user = zombie.Character(name, health=90, attack=20, score=0, max_health=90, max_attack=20)
             print(f'Hi, {user.name}, your health is {user.health} and attack is {user.attack}.')
-            great_text(f"HP:{user.health}      DMG: {user.attack}","magenta","letterw3")
+            great_text(f"HP:{user.health}     DMG: {user.attack}","magenta","letterw3")
             break
         else:
             print('Please make sure you type the correct gender for your character. ')
