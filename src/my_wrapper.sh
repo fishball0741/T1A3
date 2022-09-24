ed4da11a1428765f0cbdbac55c35d7f4e54eb798
fishball0741 <fishball0741@gmail.com>
2022-09-24 20:38:38 +1000

    add lib1

--- src/my_wrapper.sh
+++ src/my_wrapper.sh
@@ -4,8 +4,8 @@ a=$(python3 --version)
 if  [[ "$a" > 3 ]]
 then
     echo python "$a" is installed running the application...
-    python3 -m venv .venv
-    source .venv/bin/activate
+    python3 -m venv env
+    source env/bin/activate
     pip3 install -r requirements.txt
     python3 main.py "$@"
 else
